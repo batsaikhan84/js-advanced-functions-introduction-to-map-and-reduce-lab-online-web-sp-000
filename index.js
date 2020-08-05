478a90256ffb3d23fba26803e82538300298de4f
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-08-05 23:26:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+function mapToNegativize(sourceArray) {
+  return sourceArray.map(item => item * -1)
+}
+
+function mapToNoChange(sourceArray) {
+  return sourceArray.map(item => item)
+}
+
+function mapToDouble(sourceArray) {
+  return sourceArray.map(item => item * 2)
+}
+
+function mapToSquare(sourceArray) {
+  return sourceArray.map(item => item * item)
+}
+
+function reduceToTotal(sourceArray, startingPoint) {
+    sourceArray.reduce(function(a, b) {
+      a + b
+    }, 0)
+    return a
+}
